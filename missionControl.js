b208a7b20d5e0bb6ca7826f2541d036e113db519
PSalatrik <philip.salatrik@gmail.com>
2015-12-01 17:02:30 -0500

    better names and ng-repeat

--- missionControl.js
+++ missionControl.js
@@ -2,7 +2,9 @@ var app = angular.module("myMod");
 
 app.controller('missionControl', function($scope, redditService) {
 
-		$scope.redditPayload = redditService.getContent();
-		console.log($scope.redditPayload);
+		$scope.redditPayload = redditService.getContent().data.children;
+		console.log($scope.redditPayload.data.children[0].data);
+
+	
 
 });
