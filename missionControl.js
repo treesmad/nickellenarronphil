22c487a0d224bbaea339995e8edf3fd856621675
Ellen Zimmer <ellenzimmer711@gmail.com>
2015-12-01 17:07:26 -0500

    change to controller

--- missionControl.js
+++ missionControl.js
@@ -2,6 +2,9 @@ var app = angular.module("myMod");
 
 app.controller('missionControl', function($scope, redditService) {
 
-	$scope.redditPayload = redditService.getContent().data.children;
+		$scope.redditPayload = redditService.getContent();
+		console.log($scope.redditPayload.data.children[1].data);
+
+	var posts = response.data.children;
 
 });
