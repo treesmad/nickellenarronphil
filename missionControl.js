c5db35a4c5a5b11f6d650c42314f0d7cc1e42a3e
adesanto <aarondesanto@gmail.com>
2015-12-01 17:08:34 -0500

    merge conflicts again

--- missionControl.js
+++ missionControl.js
@@ -2,9 +2,6 @@ var app = angular.module("myMod");
 
 app.controller('missionControl', function($scope, redditService) {
 
-		$scope.redditPayload = redditService.getContent();
-		console.log($scope.redditPayload.data.children[1].data);
-
-	var posts = response.data.children;
+		$scope.redditPayload = redditService.getContent().data.children;
 
 });
